03eacd92192b7abaf09fb3d1ec0a067e85a71784
Sol Grassi <grassisolgianna@gmail.com>
2025-04-14 15:50:08 -0300

    Create Nodo.java

---
+++ Practica2/Ejercicio9/Nodo.java
@@ -0,0 +1,19 @@
+package tp2_Ejercicio9;
+
+public class Nodo {
+private int suma;
+private int diferencia;
+
+public int getSuma() {
+	return suma;
+}
+public void setSuma(int suma) {
+	this.suma = suma;
+}
+public int getDiferencia() {
+	return diferencia;
+}
+public void setDiferencia(int diferencia) {
+	this.diferencia = diferencia;
+}	
+}
